a8e31eaf1915073bc80c8f3c4546efd25daa6ef8
Panayotis Katsaloulis <panayotis@panayotis.com>
2017-05-24 00:14:50 +0300

    update windows terminal

--- src/main/java/com/panayotis/jerminal/TermWindows.java
+++ src/main/java/com/panayotis/jerminal/TermWindows.java
@@ -16,28 +16,20 @@ class TermWindows {
     private static int width = -1;
 
     public synchronized static int width() {
-        if (width < 0)
-            width = modeWidth();
+        if (width < 0) {
+            String res = SimpleLaunch.run(null, INHERIT, "cmd.exe", "/c", "mode con");
+            if (res != null) {
+                String[] s = res.split("\\r?\\n");
+                try {
+                    int delta = (s[0].trim().isEmpty()) ? 1 : 0;
+                    if (s[1 + delta].contains("---"))
+                        width = Integer.parseInt(s[3 + delta].substring(s[3 + delta].indexOf(':') + 1).trim());
+                } catch (Exception e) {
+                    width = 80;
+                }
+            }
+        }
         return width;
     }
 
-    private static int dllWidth() {
-        return LoadLib.load("/com/panayotis/jerminal/ntv/termwin" + SystemDependent.arch + ".dll") ? getWidth() : 80;
-    }
-
-    private static int modeWidth() {
-        String res = SimpleLaunch.run(null, INHERIT, "cmd.exe", "/c", "mode con");
-        if (res != null) {
-            String[] s = res.split("\\r?\\n");
-            try {
-                int delta = (s[0].trim().isEmpty()) ? 1 : 0;
-                if (s[1 + delta].contains("---"))
-                    return Integer.parseInt(s[3 + delta].substring(s[3 + delta].indexOf(':') + 1).trim());
-            } catch (Exception e) {
-            }
-        }
-        return dllWidth();
-    }
-
-    private static native int getWidth();
 }
